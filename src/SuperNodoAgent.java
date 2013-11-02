c6130730b2fe7407dc14e6c183b58fed1dd068ef
Javier Arguello <jarguellososa@gmail.com>
2013-11-02 14:24:19 -0430

    SuperNodo error envio Arraylist

--- src/SuperNodoAgent.java
+++ src/SuperNodoAgent.java
@@ -183,167 +183,166 @@ public class SuperNodoAgent extends Agent {
                         reply.setPerformative(ACLMessage.PROPAGATE);
                         // Le enviamos en el mensaje el catalogo
                         reply.setContentObject(catalogo);
-                        myAgent.send(reply);
-                    }
-
-                }catch (Exception io){
-                    io.printStackTrace();
-                }
-            }else {
-                block();
-            }
-
-            block();
-            mt = MessageTemplate.and(
-                    MessageTemplate.MatchPerformative(ACLMessage.INFORM),
-                    MessageTemplate.MatchConversationId("listaSuperNodos"));
-            msg = myAgent.receive(mt);
-            if (msg != null) {
-                try {
-                    reply = msg.createReply();
-                    reply.setPerformative(ACLMessage.PROPAGATE);
-                    reply.setContentObject(superNodos);
-                    reply.setConversationId("listaSuperNodos");
-                    myAgent.send(reply);
-                } catch (Exception io) {
-                    io.printStackTrace();
-                }
-            }
-        }
-    }
-
-    private class addSuperNode extends CyclicBehaviour {
-        public void action() {
-            MessageTemplate mt =  MessageTemplate.and(  
-                    MessageTemplate.MatchPerformative(ACLMessage.INFORM),
-                    MessageTemplate.MatchConversationId("agregarSuperNodo"));
-            ACLMessage msg = myAgent.receive(mt);
-            if (msg != null) {
-                try {
-                    AID nuevo = msg.getSender();
-                    superNodos.add(nuevo);
-                } catch (Exception io) {
-                    io.printStackTrace();
-                }
-            }
-        }
-    }
-
-
-    /* 	
-        Metodo Propagate
-        Este metodo se encarga de actualizar la Tabla de Hash ya que el cliente tiene
-        un nuevo recurso que desea compartir
-        */
-    private class Propagate extends CyclicBehaviour {
-        public void action() {
-            // Recibimos el mensaje que puede venir de un Nodo
-            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
-            ACLMessage msg = myAgent.receive(mt);
-
-            if (msg != null) {
-                Fichero arch;
-                Fichero arch2;
-                try {
-                    if(msg.getContent().equalsIgnoreCase("NuevoArchivo")){
-                        /** Espacio para  actualizar */
-                        //Nuevo Archivo se recibe desde el cliente y se guarda
-
-                        arch = (Fichero)msg.getContentObject();
-                        catalogo.put(arch.getNombre(),arch);
-                    }else if(msg.getContent().equalsIgnoreCase("NuevoPermiso")){
-                        //Cambio de Permisos de un archivo se recibe el archivo desde el cliente
-                        arch = (Fichero) msg.getContentObject();
-                        arch2 = (Fichero) catalogo.get(arch.getNombre());
-                        //Actualizar la lista de holders
-                        arch.setHolders(arch2.getHolders());
-                        catalogo.put(arch.getNombre(),arch);
-                    }else{
-                        //Nuevo nodo con el archivo
-                        arch = (Fichero) msg.getContentObject();
-                        arch2 = (Fichero) catalogo.get(arch.getNombre());
-                        arch2.setAHolder(arch.getOwner());
-                        catalogo.put(arch.getNombre(),arch2);
-                    }
-
-
-
-                    DFAgentDescription template = new DFAgentDescription();
-                    ServiceDescription sd = new ServiceDescription();
-                    sd.setType("supernodo");
-                    template.addServices(sd);
-                    // Buscamos todos los agentes que posean el servicio SuperNodo
-                    DFAgentDescription[] result = DFService.search(myAgent, template); 
-                    AID[] superNodos = new AID[result.length];
-
-                    //Por cada superNodo le enviamos la nueva Tabla de Hash
-                    for (int i = 0; i < result.length; ++i) {
-                        superNodos[i] = result[i].getName();
-                        ACLMessage cfp = new ACLMessage(ACLMessage.PROPAGATE);
-                        cfp.addReceiver(superNodos[i]);
-                        cfp. setContentObject(catalogo);
-                        cfp.setConversationId("propagate");
-                        cfp.setReplyWith("cfp"+System.currentTimeMillis()); // Unique value
-                        myAgent.send(cfp);
-                        System.out.println(superNodos[i].getName());
-                    }
-
-                }catch (FIPAException fe) {
-                    fe.printStackTrace();
-                }catch (Exception io){
-                    io.printStackTrace();
-                }
-            }else {
-                block();
-            }
-        }
-    }
-
-    /* Metodo Actualizar
-       Este metodo se encarga de actualizar la Tabla de Hash debido a una notificacion de 
-       actualizacion, desechamos la tabla anterior y asignamos como nueva la recibida en
-       el mensaje
-       */
-    private class Actualizar extends CyclicBehaviour {
-        int step = 0; 
-        public void action() {
-
-            MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.PROPAGATE);
-            ACLMessage msg = myAgent.receive(mt);
-
-            switch(step){
-                case 0: 
-                    if (msg != null) {
-                        // Mensaje recibido.
-                        //Archivo pedido
-                        try {
-                            // En el mensaje se encuentra la tabla de Hash
-                            Hashtable contenido = (Hashtable)msg.getContentObject();
-                            // Esta sera nuestra nueva tabla de hash
-                            catalogo = contenido;
-                            System.out.println("Tabla de Hash Actualizada\n");
-                            step = 1;
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                            myAgent.send(reply);
                         }
-                    } else {
-                        block();
+
+                    }catch (Exception io){
+                        io.printStackTrace();
                     }
-
-                    break;
-                case 1:
-                    msg = new ACLMessage();
-                    msg.setPerformative(ACLMessage.INFORM);
-                    msg.setConversationId("listaSuperNodos");
-                    msg.setContent("listaSuperNodos");
-                    myAgent.send(msg);
+                }else {
                     block();
+                }
 
-                    mt = MessageTemplate.and(
-                            MessageTemplate.MatchPerformative(ACLMessage.INFORM),
-                            MessageTemplate.MatchConversationId("listaSuperNodos"));
-                    msg = myAgent.receive(mt);
-                        try {
+                block();
+                mt = MessageTemplate.and(
+                        MessageTemplate.MatchPerformative(ACLMessage.INFORM),
+                        MessageTemplate.MatchConversationId("listaSuperNodos"));
+                msg = myAgent.receive(mt);
+                    try {
+                        reply = msg.createReply();
+                        reply.setPerformative(ACLMessage.PROPAGATE);
+                        System.out.println("Lista " + superNodos.toString());
+                        reply.setContentObject(superNodos);
+                        reply.setConversationId("listaSuperNodos");
+                        myAgent.send(reply);
+                    } catch (Exception io) {
+                        io.printStackTrace();
+                    }
+            }
+        }
+
+        private class addSuperNode extends CyclicBehaviour {
+            public void action() {
+                MessageTemplate mt =  MessageTemplate.and(  
+                        MessageTemplate.MatchPerformative(ACLMessage.INFORM),
+                        MessageTemplate.MatchConversationId("agregarSuperNodo"));
+                ACLMessage msg = myAgent.receive(mt);
+                if (msg != null) {
+                    try {
+                        AID nuevo = msg.getSender();
+                        superNodos.add(nuevo);
+                    } catch (Exception io) {
+                        io.printStackTrace();
+                    }
+                }
+            }
+        }
+
+
+        /* 	
+            Metodo Propagate
+            Este metodo se encarga de actualizar la Tabla de Hash ya que el cliente tiene
+            un nuevo recurso que desea compartir
+            */
+        private class Propagate extends CyclicBehaviour {
+            public void action() {
+                // Recibimos el mensaje que puede venir de un Nodo
+                MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
+                ACLMessage msg = myAgent.receive(mt);
+
+                if (msg != null) {
+                    Fichero arch;
+                    Fichero arch2;
+                    try {
+                        if(msg.getContent().equalsIgnoreCase("NuevoArchivo")){
+                            /** Espacio para  actualizar */
+                            //Nuevo Archivo se recibe desde el cliente y se guarda
+
+                            arch = (Fichero)msg.getContentObject();
+                            catalogo.put(arch.getNombre(),arch);
+                        }else if(msg.getContent().equalsIgnoreCase("NuevoPermiso")){
+                            //Cambio de Permisos de un archivo se recibe el archivo desde el cliente
+                            arch = (Fichero) msg.getContentObject();
+                            arch2 = (Fichero) catalogo.get(arch.getNombre());
+                            //Actualizar la lista de holders
+                            arch.setHolders(arch2.getHolders());
+                            catalogo.put(arch.getNombre(),arch);
+                        }else{
+                            //Nuevo nodo con el archivo
+                            arch = (Fichero) msg.getContentObject();
+                            arch2 = (Fichero) catalogo.get(arch.getNombre());
+                            arch2.setAHolder(arch.getOwner());
+                            catalogo.put(arch.getNombre(),arch2);
+                        }
+
+
+
+                        DFAgentDescription template = new DFAgentDescription();
+                        ServiceDescription sd = new ServiceDescription();
+                        sd.setType("supernodo");
+                        template.addServices(sd);
+                        // Buscamos todos los agentes que posean el servicio SuperNodo
+                        DFAgentDescription[] result = DFService.search(myAgent, template); 
+                        AID[] superNodos = new AID[result.length];
+
+                        //Por cada superNodo le enviamos la nueva Tabla de Hash
+                        for (int i = 0; i < result.length; ++i) {
+                            superNodos[i] = result[i].getName();
+                            ACLMessage cfp = new ACLMessage(ACLMessage.PROPAGATE);
+                            cfp.addReceiver(superNodos[i]);
+                            cfp. setContentObject(catalogo);
+                            cfp.setConversationId("propagate");
+                            cfp.setReplyWith("cfp"+System.currentTimeMillis()); // Unique value
+                            myAgent.send(cfp);
+                            System.out.println(superNodos[i].getName());
+                        }
+
+                    }catch (FIPAException fe) {
+                        fe.printStackTrace();
+                    }catch (Exception io){
+                        io.printStackTrace();
+                    }
+                }else {
+                    block();
+                }
+            }
+        }
+
+        /* Metodo Actualizar
+           Este metodo se encarga de actualizar la Tabla de Hash debido a una notificacion de 
+           actualizacion, desechamos la tabla anterior y asignamos como nueva la recibida en
+           el mensaje
+           */
+        private class Actualizar extends CyclicBehaviour {
+            int step = 0; 
+            public void action() {
+
+                MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.PROPAGATE);
+                ACLMessage msg = myAgent.receive(mt);
+
+                switch(step){
+                    case 0: 
+                        if (msg != null) {
+                            // Mensaje recibido.
+                            //Archivo pedido
+                            try {
+                                // En el mensaje se encuentra la tabla de Hash
+                                Hashtable contenido = (Hashtable)msg.getContentObject();
+                                // Esta sera nuestra nueva tabla de hash
+                                catalogo = contenido;
+                                System.out.println("Tabla de Hash Actualizada\n");
+                                step = 1;
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            block();
+                        }
+
+                        break;
+                    case 1:
+                        msg = new ACLMessage();
+                        msg.setPerformative(ACLMessage.INFORM);
+                        msg.setConversationId("listaSuperNodos");
+                        msg.setContent("listaSuperNodos");
+                        myAgent.send(msg);
+                        block();
+
+                        mt = MessageTemplate.and(
+                                MessageTemplate.MatchPerformative(ACLMessage.INFORM),
+                                MessageTemplate.MatchConversationId("listaSuperNodos"));
+                        msg = myAgent.receive(mt);
+                            try {
                             superNodos = (ArrayList<AID>) msg.getContentObject();
                             System.out.println("Lista de super nodos Actualizada\n");
                             step = 2;
